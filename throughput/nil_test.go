60152dc774eb5e1ab00b5868bfa4f664112350a4
Rob Kingsbury <rob.kingsbury@gmail.com>
2020-10-10 23:43:02 -0700

    Added TestNilReport()

--- throughput/nil_test.go
+++ throughput/nil_test.go
@@ -10,3 +10,11 @@ func TestNilReporterImplementsReporter(t *testing.T) {
 	n = &NilReporter{}
 	t.Logf("%v", n)
 }
+
+// Report should always return an error.
+func TestNilReport(t *testing.T) {
+	n := &NilReporter{}
+	if _, err := n.Report(); err == nil {
+		t.Errorf("Report() on a NilReporter should always return an error")
+	}
+}
