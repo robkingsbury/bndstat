28a5757500b4aef1e11cd0975a28a9e4b6e40794
Rob Kingsbury <rob.kingsbury@gmail.com>
2020-10-14 22:20:13 -0700

    Added interval and count flags and args

--- bndstat.go
+++ bndstat.go
@@ -14,18 +14,32 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/golang/glog"
 	"github.com/robkingsbury/bndstat/throughput"
 )
 
+var countFlag = flag.Int("count", 0, "count of updates, any zero or negative values are considered infinity")
+var intervalFlag = flag.Int("interval", 3, "period time between updates in `seconds`")
+
 func main() {
 	flag.Parse()
 
+	// Get the interval and count, which can be specified either as standard
+	// flags are unflagged args.
+	interval, count, err := parseUnflaggedArgs(flag.Arg(0), flag.Arg(1), *intervalFlag, *countFlag)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing options: %s\n", err)
+		glog.Flush()
+		os.Exit(1)
+	}
+	glog.V(1).Infof("interval = %d, count = %d", interval, count)
+
 	devices := []string{}
 
-	if err := bndstat(devices); err != nil {
+	if err := bndstat(devices, interval, count); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		glog.Flush()
 		os.Exit(1)
@@ -35,7 +49,7 @@ func main() {
 	os.Exit(0)
 }
 
-func bndstat(devices []string) error {
+func bndstat(devices []string, interval int, count int) error {
 	t := throughput.NewTable()
 	r, err := throughput.New()
 	if err != nil {
@@ -45,6 +59,7 @@ func bndstat(devices []string) error {
 	unit := throughput.Kbps
 
 	time.Sleep(time.Second)
+	i := 1
 	for {
 		stats, err := r.Report()
 		if err != nil {
@@ -55,7 +70,40 @@ func bndstat(devices []string) error {
 		sort.Strings(devices)
 		t.Write(stats, devices, unit)
 
-		time.Sleep(3 * time.Second)
+		if count > 0 {
+			if i >= count {
+				return nil
+			}
+			i++
+		}
+
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
+
 	return nil
+}
+
+func parseUnflaggedArgs(interval string, count string, intervalFlag int, countFlag int) (int, int, error) {
+	// If the unflagged interval is empty, return the flagged values.
+	if interval == "" {
+		return intervalFlag, countFlag, nil
+	}
+
+	i, err := strconv.Atoi(interval)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// If the unflagged count is empty, return the unflagged interval but the
+	// flagged count.
+	if count == "" {
+		return i, countFlag, nil
+	}
+
+	c, err := strconv.Atoi(count)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return i, c, nil
 }
