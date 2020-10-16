7fed37ae55b8f0ae1f2fd2760701ad3a38a27101
Rob Kingsbury <rob.kingsbury@gmail.com>
2020-10-15 21:59:24 -0700

    Added --devices option

--- bndstat.go
+++ bndstat.go
@@ -1,12 +1,5 @@
 // Bndstat displays simple network device throughput data, inspired by unix
 // tools such as vmstat, iostat, mpstat, netstat, etc.
-//
-// A note about logging: bndstat uses the glog facility for logging but, under
-// default conditions i.e. no log flags provided, bndstat should not write log
-// files to log_dir. To accomplish this, all Info-like glog calls are done
-// through the V() function. This should avoid a proliferation of log files
-// being written through the normal use of the program, which also avoids
-// having to write code to clean up those log files.
 package main
 
 import (
@@ -15,6 +8,7 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/golang/glog"
@@ -22,18 +16,25 @@ import (
 )
 
 var countFlag = flag.Int("count", 0, "count of updates, any zero or negative values are considered infinity")
+var devicesFlag = flag.String("devices", "", "comma separated list of devices to output; all non-loopback devices included if empty")
 var intervalFlag = flag.Int("interval", 3, "period time between updates in `seconds`")
 
 func init() {
 	// Define a custom usage message that more pleasing to mine eye.
 	flag.Usage = func() {
 		u := "Usage: bndstat [option]... [interval [count]]\n"
-		u += "Output the average throughput of network devices over an interval\n"
+		u += "Output the average throughput of network devices over an interval.\n"
+		u += "\n"
+		u += "Interval and count have the same behavior as the options of the same\n"
+		u += "name. However, when both an option and the non-option arg are present,\n"
+		u += "the value specified in the option takes precedence.\n"
 		u += "\n"
 		u += "Options:\n"
 		u += "  --interval=seconds    Number of seconds between updates\n"
 		u += "  --count=num           Number of updates to print, any num less than one\n"
-		u += "                          will output infinite updates until \n"
+		u += "                          will output infinite updates until\n"
+		u += "  --devices=list        Comma separated list of devices to output; if empty,\n"
+		u += "                          all non-loopback devices are included\n"
 		fmt.Fprintf(flag.CommandLine.Output(), u)
 	}
 }
@@ -41,29 +42,25 @@ func init() {
 func main() {
 	flag.Parse()
 
+	if err := bndstat(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func bndstat() error {
+	defer glog.Flush()
+
 	// Get the interval and count, which can be specified either as standard
 	// flags are unflagged args.
 	interval, count, err := parseUnflaggedArgs(flag.Arg(0), flag.Arg(1), *intervalFlag, *countFlag)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error parsing options: %s\n", err)
-		glog.Flush()
-		os.Exit(1)
+		return fmt.Errorf("error parsing options: %s", err)
 	}
 	glog.V(1).Infof("interval = %d, count = %d", interval, count)
 
-	devices := []string{}
-
-	if err := bndstat(devices, interval, count); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-		glog.Flush()
-		os.Exit(1)
-	}
-
-	glog.Flush()
-	os.Exit(0)
-}
-
-func bndstat(devices []string, interval int, count int) error {
 	t := throughput.NewTable()
 	r, err := throughput.New()
 	if err != nil {
@@ -71,26 +68,36 @@ func bndstat(devices []string, interval int, count int) error {
 	}
 
 	unit := throughput.Kbps
+	devices := strings.Split(*devicesFlag, ",")
 
-	time.Sleep(time.Second)
-	i := 1
+	// Sleep for a small amount of time so that the first line output is not all
+	// zeros.
+	time.Sleep(10 * time.Millisecond)
+
+	updateCount := 1
 	for {
 		stats, err := r.Report()
 		if err != nil {
 			glog.Exitf("%s", err)
 		}
 
-		devices := stats.Devices()
+		if *devicesFlag == "" {
+			devices = []string{}
+			for _, d := range stats.Devices() {
+				if d != "lo" {
+					devices = append(devices, d)
+				}
+			}
+		}
+
 		sort.Strings(devices)
 		t.Write(stats, devices, unit)
 
-		if count > 0 {
-			if i >= count {
-				return nil
-			}
-			i++
+		if count > 0 && updateCount >= count {
+			return nil
 		}
 
+		updateCount++
 		time.Sleep(time.Duration(interval) * time.Second)
 	}
 
