a737b0b771f742c12de2d0d99c14b2f298d9a1f3
Rob Kingsbury <rob.kingsbury@gmail.com>
2023-04-01 10:46:54 -0700

    Adding gofmt to tagit.sh

--- util/tagit.sh
+++ util/tagit.sh
@@ -5,6 +5,11 @@ if [[ ${1} == "" ]]; then
   exit 1
 fi
 
+echo Fixing formatting if needed
+pushd ..
+find . -name \*.go -exec gofmt -l -w -s {} \;
+popd
+
 ./genreadme.sh ${1}
 git add ../README.md
 git commit -m "Tagging ${1}"
